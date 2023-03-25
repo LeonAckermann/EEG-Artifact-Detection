a9fa720e13eafc39c2d42d1330e155438e1ee9c8
Leon Ackermann <leonackermann@Leons-MacBook-Pro.local>
2023-03-25 13:38:52 +0800

    first commit, added file system

---
+++ mylibs.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.callbacks import TensorBoard
+from tensorboard.plugins.hparams import api as hp
+from datetime import datetime
+import tqdm
+import numpy as np
+import os
+import tempfile
+import einops
+import shutil
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import pickle
+import zipfile
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorboard as tb
