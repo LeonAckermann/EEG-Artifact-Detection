95f3dab198cf8a52d30ddde1b1a2f5a7f3f56c54
Leon Ackermann <leonackermann@Leons-MacBook-Pro.local>
2023-03-29 15:41:21 +0800

    changes to architecture and hyperparameter optimization

--- architectures/BidirectionalLSTM.py
+++ architectures/BidirectionalLSTM.py
@@ -4,21 +4,38 @@ from mylibs import *
 
 class BidirectionalLSTM(keras.Model):
 
-    def __init__(self, hidden_units, number_bidirectional_layers, activation_function='tanh'):
+    def __init__(self, 
+                 num_hidden_units, 
+                 num_bidirectional_layers, 
+                 num_dense_units=0,
+                 num_dense_layers=0,
+                 activation_function='tanh',
+                 increase=False):
+        
+        
         super(BidirectionalLSTM, self).__init__()
 
-        self.lstm_layers_list = [
-            keras.layers.Bidirectional(keras.layers.LSTM(hidden_units, 
+
+        self.lstm_layers_list = []
+        for i in range(num_bidirectional_layers):
+            self.lstm_layers_list.append(keras.layers.LSTM(num_hidden_units, 
                                                          return_sequences=True,
                                                          activation=activation_function))
-            for i in range(number_bidirectional_layers)
-        ]
-      
+        self.dense_layers_list = []  
+        self.num_dense_layers = num_dense_layers
+        if num_dense_layers > 0:
+            for i in range(num_dense_layers):
+                self.dense_layers_list.append(keras.layers.Dense(num_dense_units, activation='relu'))
+
         self.dense = keras.layers.Dense(2, activation="sigmoid")
 
     def call(self, inputs, training=False):
         x = inputs
         for layer in self.lstm_layers_list:
             x = layer(x)
+
+        if self.num_dense_layers > 0:
+            for layer in self.dense_layers_list:
+                x = layer(x)
 
         return self.dense(x)
--- architectures/MultiLayerLSTM.py
+++ architectures/MultiLayerLSTM.py
@@ -4,22 +4,41 @@ from mylibs import *
 
 class MultiLayerLSTM(keras.Model):
 
-    def __init__(self, hidden_units, number_lstm_layers, activation_function='tanh'):
+    def __init__(self, 
+                 num_hidden_units, 
+                 num_lstm_layers,
+                 num_dense_units=0,
+                 num_dense_layers=0,
+                 activation_function='tanh',
+                 increase=0):
+        
         super(MultiLayerLSTM, self).__init__()
 
-        self.lstm_layers_list = [
-            keras.layers.LSTM(hidden_units, 
-                              return_sequences=True, 
-                              activation=activation_function)
-            for i in range(number_lstm_layers)
-        ]
+
+        self.lstm_layers_list = []
+        for i in range(num_lstm_layers):
+            self.lstm_layers_list.append(keras.layers.LSTM(num_hidden_units, 
+                                                           return_sequences=True, 
+                                                           activation=activation_function))
       
+        
+
+        if num_dense_layers > 0:
+            self.dense_layers_list = []
+            self.num_dense_layers = num_dense_layers
+            for i in range(num_dense_layers):
+                self.dense_layers_list.append(keras.layers.Dense(num_dense_units, activation='relu'))
+
         self.dense = keras.layers.Dense(2, activation="sigmoid")
 
     def call(self, inputs, training=False):
         x = inputs
         for layer in self.lstm_layers_list:
             x = layer(x)
+        
+        if self.num_dense_layers > 0:
+            for layer in self.num_dense_layers:
+                x = layer(x)
         
         return self.dense(x)
       
