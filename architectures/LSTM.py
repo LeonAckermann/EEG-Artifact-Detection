ee7da110cbf53444f3bee26f49e2f7d9c57952d0
Leon Ackermann <leonackermann@Leons-MacBook-Pro.local>
2023-04-01 17:19:22 +0800

    added experiment class

---
+++ architectures/LSTM.py
@@ -0,0 +1,75 @@
+import sys
+sys.path.append('./')
+from mylibs import *
+
+class LSTM(keras.Model):
+
+    """
+      The constructor takes the following arguments:
+      num_hidden_units: Integer, the number of hidden units in lstm layer
+      num_lstm_layers: Integer, the number of LSTM layers
+      num_dense_units: Integer, the number of units in each dense layer.
+      num_dense_layers: Integer, the number of dense layers in the model.
+      num_conv_layers: Integer, the number of convolutional layers in the conv_block.
+      increase: bool, whether hidden_units increase with in higher lstm layers
+      bidirectiona: Boolean, whether bidirectional or multilayer lstm used
+      """
+
+    def __init__(self, 
+                 num_hidden_units, 
+                 num_lstm_layers, 
+                 num_dense_units=0,
+                 num_dense_layers=0,
+                 num_conv_layers=0,
+                 increase=0,
+                 bidirectional=False):
+        
+        
+        super(LSTM, self).__init__()
+
+        self.conv_layers_list = []
+        self.num_conv_layers = num_conv_layers
+        if num_conv_layers > 0:
+            for i in range(num_conv_layers):
+                self.conv_layers_list.append(layers.TimeDistributed(layers.Conv1D(filters=8*i+8, kernel_size=3, activation='relu')))
+            self.conv_layers_list.append(layers.TimeDistributed(layers.MaxPool1D()))
+            self.conv_layers_list.append(layers.TimeDistributed(layers.Flatten()))
+
+        
+        self.lstm_layers_list = []
+        if bidirectional:
+            for i in range(num_lstm_layers):
+                self.lstm_layers_list.append(keras.layers.Bidirectional(keras.layers.LSTM(num_hidden_units+(num_hidden_units*i*increase), 
+                                                                                      return_sequences=True,
+                                                                                      activation='tanh')))
+        else:
+            for i in range(num_lstm_layers):
+                self.lstm_layers_list.append(keras.layers.LSTM(num_hidden_units+(num_hidden_units*i*increase), 
+                                                               return_sequences=True, 
+                                                               activation='tanh'))
+
+        
+        self.dense_layers_list = []  
+        self.num_dense_layers = num_dense_layers
+        if num_dense_layers > 0:
+            for i in range(num_dense_layers):
+                self.dense_layers_list.append(keras.layers.Dense(num_dense_units, activation='relu'))
+
+        self.dense = keras.layers.Dense(2, activation="sigmoid")
+
+    def call(self, inputs, training=False):
+        x = inputs
+
+        if self.num_conv_layers > 0:
+            for layer in self.conv_layers_list:
+                print(x)
+                x = layer(x)
+
+        for layer in self.lstm_layers_list:
+            x = layer(x)
+
+        if self.num_dense_layers > 0:
+            for layer in self.dense_layers_list:
+                x = layer(x)
+
+        return self.dense(x)
---
+++ hyperparams_optimization/experiment.py
@@ -0,0 +1,132 @@
+import sys
+sys.path.append('./')
+from mylibs import *
+
+from architectures.LSTM import LSTM
+from architectures.Attention import CCNAttentionNetwork
+
+class Experiment:
+    def create_model(self, hparams):
+        """
+        returns a model based on the selected hyperparameters
+        """
+        models = {
+            "LSTM": LSTM(num_hidden_units = hparams['HP_NUM_HIDDEN_UNITS'], 
+                        num_lstm_layers=hparams['HP_NUM_LSTM_LAYERS'],
+                        num_dense_units = hparams['HP_NUM_DENSE_LAYERS'],
+                        num_dense_layers = hparams['HP_NUM_DENSE_UNITS'],
+                        num_conv_layers=hparams['HP_NUM_CONV_LAYERS'],
+                        increase=hparams['HP_INCREASE_UNITS_PER_LSTM_LAYER'],
+                        bidirectional=hparams['HP_BIDIRECTIONAL']),
+
+            "CNNAttention": CCNAttentionNetwork(num_units= hparams['HP_NUM_UNITS'],
+                                                num_layers=hparams['HP_NUM_LAYERS'],
+                                                num_conv_layers=hparams['HP_NUM_CONV_LAYERS'],
+                                                num_heads=hparams['HP_NUM_HEADS'],
+                                                attention=['HP_ATTENTION']) 
+        }
+
+        return models.get(hparams['HP_MODEL_ARCHITECTURE'])
+
+
+
+    def run_model(self, train, val, test, hparams, logdir, savedir, checkpointdir, metrics):
+        """
+        builds, compiles, trains and evaluates a model with certain architectual hyperparameters
+    
+        Args:
+            hparams: selected hyperparameters
+            logdir: directory for logs
+            savedir: directors for saving the model
+            checkpointdir: direcotry for model checkpoints
+            metrics: a list of metrics we want to track
+    
+        Returns:
+            accuracy of trained model evaluated on test data set
+        """
+        model = self.create_model(hparams)
+        model.compile(
+            optimizer='adam',
+            loss=tf.keras.losses.BinaryCrossentropy(),
+            metrics=metrics)
+    
+    
+        model.fit(train,
+                  batch_size=64,
+                  epochs=1,
+                  validation_data=val,
+                  verbose=0, # no output during training
+                  callbacks=[tf.keras.callbacks.TensorBoard(logdir),  # log metrics
+                            hp.KerasCallback(logdir, hparams),  # log hparams
+                            tf.keras.callbacks.ModelCheckpoint(filepath= os.path.join(checkpointdir, "ckpt_{epoch}") ,monitor='val_loss',save_weights_only=True), # save checkpoints when val loss goes down
+                            keras.callbacks.EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True)], # early stopping in the case that loss doesnt go down for 3 epochs
+                  ) 
+        results = model.evaluate(test)
+        model.save(savedir)
+        return results
+    
+
+    def run_experimen(self, hparams_dict, logdir, metrics):
+        """
+        train models on the selected hyperparamers and log the metrics, checkpoints and trained model in logidr
+        """
+        if hparams_dict['HP_MODEL_ARCHITECTURE']=="CNNAttention":
+            session_num = 0
+            for num_units in hparams_dict['HP_NUM_UNITS'].domain.values:
+                for num_layers in hparams_dict['HP_NUM_LAYERS'].domain.values:
+                    for num_heads in hparams_dict['HP_NUM_HEADS'].domain.values:
+                        for num_convlayers in hparams_dict['HP_NUM_CONVLAYERS'].domain.values:
+                            for attention in hparams_dict['HP_ATTENTION'].domain.values:
+                                for architecture in hparams_dict['HP_ARCHITECTURE'].domain.values:
+                                    hparams = {
+                                        'HP_NUM_UNITS': num_units,
+                                        'HP_NUM_LAYERS': num_layers,
+                                        'HP_NUM_HEADS': num_heads,
+                                        'HP_NUM_CONV_LAYERS': num_convlayers,
+                                        'HP_ATTENTION': attention,
+                                        'HP_ARCHITECTURE': architecture
+                                    }
+                                    run_name = "run-%d" % session_num
+                                    print('--- Starting trial: %s' % run_name)
+                                    print({h.name: hparams[h] for h in hparams})
+
+                                    # Run a single experiment
+                                    accuracy = self.run_model(
+                                        hparams= hparams,
+                                        epochs = 20,
+                                        logdir=logdir+'/hparam_tuning/' + run_name, 
+                                        savedir=logdir+'logs/models/'+run_name, 
+                                        checkpointdir=logdir+'logs/checkpoints'+run_name)
+                                    session_num += 1
+
+
+        if hparams_dict['HP_MODEL_ARCHITECTURE']=="LSTM":
+            session_num = 0
+            for num_lstm_layers in hparams_dict['HP_NUM_LSTM_LAYERS'].domain.values:
+                for num_hidden_units in hparams_dict['HP_NUM_HIDDEN_UNITS'].domain.values:
+                    for num_conv_layers in hparams_dict['HP_NUM_CONV_LAYERS'].domain.values:
+                        for num_dense_layers in hparams_dict['HP_NUM_DENSE_LAYERS'].domain.values:
+                            for num_dense_units in hparams_dict['HP_NUM_DENSE_UNITS'].domain.values:
+                                for increase in hparams_dict['HP_INCREASE_UNITS_PER_LSTM_LAYER'].domain.values:
+                                    for model_architecture in hparams_dict['HP_MODEL_ARCHITECTURE'].domain.values:
+                                        for bidirectional in hparams_dict['HP_BIDIRECTIONAL'].domain.values:
+                                            hparams = {
+                                                'HP_MODEL_ARCHITECTURE': model_architecture,
+                                                'HP_NUM_LSTM_LAYERS': num_lstm_layers,
+                                                'HP_NUM_HIDDEN_UNITS': num_hidden_units,
+                                                'HP_NUM_CONV_LAYERS': num_conv_layers,
+                                                'HP_NUM_DENSE_LAYERS': num_dense_layers,
+                                                'HP_NUM_DENSE_UNITS': num_dense_units,
+                                                'HP_INCREASE_UNITS_PER_LSTM_LAYER': increase,
+                                                'HP_BIDIRECTIONAL': bidirectional
+                                            }
+                                            run_name = "run-%d" % session_num
+                                            print('--- Starting trial: %s' % run_name)
+                                            print({h: hparams[h] for h in hparams})
+                                            results = self.run_experiment(logdir=logdir+'hparam_tuning/' + run_name, 
+                                                          hparams=hparams, 
+                                                          savedir=logdir+'models/'+run_name, 
+                                                          checkpointdir=logdir+'checkpoints'+run_name,
+                                                          metrics=metrics)
+                                            session_num += 1
+                                            
