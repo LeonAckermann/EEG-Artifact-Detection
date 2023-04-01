c9d7991e483fb74654c1387be46a03c0d247dd19
Leon Ackermann <leonackermann@Leons-MacBook-Pro.local>
2023-04-01 22:52:16 +0800

    changed name

--- architectures/CNNAttention.py
+++ architectures/CNNAttention.py
@@ -5,7 +5,7 @@ from mylibs import *
 # CCNAttentionNetwork is a custom Keras model that combines a 1D convolutional neural network (CNN)
 # with multi-head attention mechanism and fully connected (dense) layers.
 
-class CCNAttentionNetwork(tf.keras.Model):
+class CNNAttention(tf.keras.Model):
 
     """
     The constructor takes the following arguments:
@@ -19,7 +19,7 @@ class CCNAttentionNetwork(tf.keras.Model):
     def __init__(self, num_units, num_layers, num_heads, num_conv_layers, attention):
 
         # Call the constructor of the parent class (tf.keras.Model)
-        super(CCNAttentionNetwork, self).__init__()
+        super(CNNAttention, self).__init__()
 
         # Initialize a Keras Sequential model for the convolutional block
         self.conv_block = tf.keras.Sequential()
@@ -36,8 +36,8 @@ class CCNAttentionNetwork(tf.keras.Model):
         for i in range(num_layers):
             self.dense_layers.add(tf.keras.layers.Dense(num_units, activation='relu'))
 
-        # Initialize the attention attribute
-        self.attention = attention
+        # Initialize the attention attribute (not used in this implementation)
+        self.attention = None
 
         # Initialize a MultiHeadAttention layer with the specified number of heads and key/query/value dimensions
         self.mha = tf.keras.layers.MultiHeadAttention(num_heads, 640)
@@ -45,30 +45,17 @@ class CCNAttentionNetwork(tf.keras.Model):
         # Initialize a Dropout layer with a dropout rate of 0.3
         self.dropout = tf.keras.layers.Dropout(0.3)
 
-        # Initialize two dense layers (one per artifact type) with 640 units and a sigmoid activation function
+        # Initialize a Dense layer with 640 units and a sigmoid activation function
         self.dense = tf.keras.layers.Dense(640, activation="sigmoid")
-        self.dense2 = tf.keras.layers.Dense(640, activation = "sigmoid")
-    
+   
 
-  def call(self, x):
-    
-    
-        x = self.conv_block(x)
-                
+    def call(self, x):
+        x = self.conv_block(x)  
         if self.attention == True:
-        
             x = self.mha(x, x)
-            x = self.dropout(x)
-
-
+            x = self.dropout(x) 
         x = self.dense_layers(x)
+        output_muscle = self.dense(x)
+        output_muscle = tf.math.reduce_mean(output_muscle, axis=1)       
+        return output_muscle    
 
-        output_eye = self.dense(x)
-        output_muscle = self.dense2(x)
-
-
-
-        output_eye = tf.math.reduce_mean(output_eye, axis=1)
-        output_muscle = tf.math.reduce_mean(output_muscle, axis=1)
-
-        return output_eye, output_muscle
