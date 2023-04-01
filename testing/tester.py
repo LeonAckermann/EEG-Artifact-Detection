da10eb4afbaa70f6d123dab3f69256f67f56f8fe
Leon Ackermann <leonackermann@Leons-MacBook-Pro.local>
2023-04-01 22:27:49 +0800

    comments

--- testing/tester.py
+++ testing/tester.py
@@ -5,12 +5,18 @@ from mylibs import *
 class Tester:
     
     def load_model(self, logdir, ckptdir=None):
+        """
+        load a model from the logdir with the best checkpoints if provided
+        """
         model = keras.models.load_model(logdir)
         if ckptdir!=None:
             model.load_weights(ckptdir)
         return model
     
     def get_metric(self, metric, label, pred):
+        """
+        is used for the function get_metrics and computes the passed metric between the label and pred
+        """
         m = metric
         m.update_state(label, pred)
         result = m.result().numpy()
@@ -18,6 +24,9 @@ class Tester:
         return result
     
     def get_f1(self, precision, recall):
+        """
+        computes the f1 score based on the passed precision and recall
+        """
         return 2*precision*recall*(precision+recall)
     
     def get_metrics(self, model, test_features, test_labels):
@@ -33,19 +42,22 @@ class Tester:
         prediction_musc_bin = tf.cast(tf.greater(prediction_musc, 0.5), tf.int32)
         prediction_eyem_bin = tf.cast(tf.greater(prediction_eyem, 0.5), tf.int32)
 
+        # compute accuracies
+        accuracy = self.get_metric(tf.keras.metrics.BinaryAccuracy(), test_labels, predictions_bin) # for both artifacts
+        accuracy_musc = self.get_metric(tf.keras.metrics.BinaryAccuracy(), test_labels_musc, prediction_musc_bin) # only for muscler artifacts 
+        accuracy_eyem = self.get_metric(tf.keras.metrics.BinaryAccuracy(), test_labels_eyem, prediction_eyem_bin) # only for eye movement artifacts
 
-        accuracy = self.get_metric(tf.keras.metrics.BinaryAccuracy(), test_labels, predictions_bin)
-        accuracy_musc = self.get_metric(tf.keras.metrics.BinaryAccuracy(), test_labels_musc, prediction_musc_bin)
-        accuracy_eyem = self.get_metric(tf.keras.metrics.BinaryAccuracy(), test_labels_eyem, prediction_eyem_bin)
+        # compute precision scores
+        precision = self.get_metric(tf.keras.metrics.Precision(), test_labels, predictions_bin) # for both artifacts
+        precision_musc = self.get_metric(tf.keras.metrics.Precision(), test_labels_musc, prediction_musc_bin) # only for muscle artifacts
+        precision_eyem = self.get_metric(tf.keras.metrics.Precision(), test_labels_eyem, prediction_eyem_bin) # only for eye movement artifacts
 
-        precision = self.get_metric(tf.keras.metrics.Precision(), test_labels, predictions_bin)
-        precision_musc = self.get_metric(tf.keras.metrics.Precision(), test_labels_musc, prediction_musc_bin)
-        precision_eyem = self.get_metric(tf.keras.metrics.Precision(), test_labels_eyem, prediction_eyem_bin)
-
+        # compute recall scores
         recall = self.get_metric(tf.keras.metrics.Recall(), test_labels, predictions_bin)
         recall_musc = self.get_metric(tf.keras.metrics.Recall(), test_labels_musc, prediction_musc_bin)
         recall_eyem = self.get_metric(tf.keras.metrics.Recall(), test_labels_eyem, prediction_eyem_bin)
 
+        # compute f1 scores
         f1 = self.get_f1(precision=precision, recall=recall)
         f1_musc = self.get_f1(precision=precision_musc, recall=recall_musc)
         f1_eyem = self.get_f1(precision=precision_eyem, recall=recall_eyem)
